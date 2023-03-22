84e23fc61445897ae1ca8aaef7da531f8473fdab
Muhammed Sahal K C <msahalkc@gmail.com>
2023-03-22 21:34:39 +0530

    avoided displaying 0 when there is no entry

--- script.js
+++ script.js
@@ -9,19 +9,39 @@ poundInput.addEventListener('input', updateKIandOI)
 
 ounceInput.addEventListener('input', updateKIandPI)
 
+
+
 function updatePIandOI() {
-    poundInput.value = kilogramInput.value * 2.205
-    ounceInput.value = kilogramInput.value * 35.274
+    if (kilogramInput.value === "") {
+        kilogramInput.value = ""
+        poundInput.value = ""
+        ounceInput.value = ""
+    }else{
+        poundInput.value = kilogramInput.value * 2.205
+        ounceInput.value = kilogramInput.value * 35.274
+    }
 }
 
 function updateKIandOI() {
-    kilogramInput.value = poundInput.value / 2.205
-    ounceInput.value = poundInput.value * 16
+    if (poundInput.value === "") {
+        kilogramInput.value = ""
+        poundInput.value = ""
+        ounceInput.value = ""
+    }else{
+        kilogramInput.value = poundInput.value / 2.205
+        ounceInput.value = poundInput.value * 16
+    }
 }
 
 function updateKIandPI() {
-    kilogramInput.value = ounceInput.value / 35.274
-    poundInput.value = ounceInput.value / 16
+    if (ounceInput.value === "") {
+        kilogramInput.value = ""
+        poundInput.value = ""
+        ounceInput.value = ""
+    }else{
+        kilogramInput.value = ounceInput.value / 35.274
+        poundInput.value = ounceInput.value / 16
+    }
 }
 
 
